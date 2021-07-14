3a0efa2dd52131323ddf84a48e818d7993cd13ac
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2021-07-14 06:05:09 -0700

    Add a node at the last of the linked list

--- LinkedList1.java
+++ LinkedList1.java
@@ -1,28 +1,44 @@
 package DS.com;
-public class LinkedList1{
+import java.util.Scanner;
+public class LinkedList1 {
     public Node start;
-    public static class Node{
-        int data;
+    public static class Node {
+        String data;
         Node next;
-        public Node(int value)
-        {
-            this.data=value;
-            this.next=null;
-        }
-    }
-    void diplay()
-    {
-        Node temp=start;
-        while(temp!=null)
-        {
-            System.out.println(temp.data);
-            temp=temp.next;
-        }
-    }
-    public static void main(String[] args) {
-        LinkedList1 li=new LinkedList1();
-       li.start= new Node(12);
-       li.diplay();
 
+        public Node(String value) {
+            this.data = value;
+            this.next = null;
+        }
     }
-}
+    public void insertAtLast(String value) {
+        Node n1 = new Node(value);
+        if (start == null) {
+            start = n1;
+            return;
+        } else {
+            Node current = start;
+            while (current != null) {
+                current = current.next;
+            }
+            current.next=n1;
+
+        }
+    }
+        void diplay()
+        {
+            Node temp = start;
+            while (temp != null) {
+                System.out.println(temp.data);
+                temp = temp.next;
+            }
+        }
+
+        public static void main (String[]args){
+            LinkedList1 li = new LinkedList1();
+            li.start = new Node("aarif");
+           li.insertAtLast("billa");
+            li.diplay();
+        }
+    }
+
